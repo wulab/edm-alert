259cde42dd3e3ace7d9516736b7ae848de781a23
Weera Wu <weera@oozou.com>
2016-10-09 20:12:01 +0700

    Remove unused models

--- app/models/dope.rb
+++
@@ -1,6 +0,0 @@
-class Dope < ApplicationRecord
-  belongs_to :post, counter_cache: true
-  belongs_to :user
-
-  validates :post_id, uniqueness: { scope: :user_id }
-end
--- app/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product < ApplicationRecord
-  # https://github.com/rails/rails/issues/18233#issuecomment-221201414
-  belongs_to :post, optional: true
-
-  validates :name, presence: true
-  validates :position, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :x, :y, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 1.0 }, allow_nil: true
-end
