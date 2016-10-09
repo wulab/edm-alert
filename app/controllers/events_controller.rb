ae9524b06e1068e5e3816118ab60d2feb12adc1a
Weera Wu <weera@oozou.com>
2016-10-09 14:42:33 +0700

    Fix province filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,21 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
-    @event_categories = Event.order('category asc').uniq.pluck(:category);
+    @events = Event.order(start_at: :desc)
+    @event_categories = Event.categories.keys
     @event_provinces = Location.joins(:events).pluck(:province).uniq
   end
 
   def category
-    @events = Event.where(category: params[:category]).order('start_at desc')
-    @event_categories = Event.where.not(category: params[:category]).uniq.pluck(:category)
+    @events = Event.where(category: params[:category]).order(start_at: :desc)
+    @event_categories = Event.categories.keys - [params[:category]]
     @event_provinces = Location.joins(:events).pluck(:province).uniq
     render :index
   end
 
   def location
-    location = Location.find_by(province: params[:location])
-    @events = location.events.order('start_at desc')
+    @events = Event.joins(:location).where(locations: { province: params[:location] }).order(start_at: :desc)
     @event_provinces = Location.joins(:events).pluck(:province).uniq
-    @event_categories = Event.uniq.pluck(:category)
+    @event_categories = Event.categories.keys
     render :index
   end
 end
