9a83599cbf1b970fb02c5b6fa3ffe7802ce87d04
Weera Wu <weera@oozou.com>
2016-10-19 15:21:19 +0700

    Change project name to EDMAlert

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,14 +54,14 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hackathon_rails_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "edm_alert_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] || 'hackathon_rails.com' }
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] || 'edm_alert.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_hackathon_rails_session'
+Rails.application.config.session_store :cookie_store, key: '_edm_alert_session'
