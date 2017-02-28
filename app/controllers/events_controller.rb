e4a38d4d49c5113ca990788b5745a635d4b211a7
eakmotion <eakpun@gmail.com>
2017-02-28 14:48:57 +0700

    Refactor controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,14 +15,17 @@ class EventsController < ApplicationController
   end
 
   def category
-    @events = Event.where(category: params[:category]).order(start_at: :desc).page(params[:page]).this_weeks
+    @events = Event.where(category: params[:category]).
+                order(start_at: :desc).page(params[:page]).this_weeks
     @event_categories = Event.categories.keys - [params[:category]]
     @event_provinces = Location.joins(:events).pluck(:province).uniq
     render :index
   end
 
   def location
-    @events = Event.joins(:location).where(locations: { province: params[:location] }).order(start_at: :desc).page(params[:page]).this_weeks
+    @events = Event.joins(:location).
+                where(locations: { province: params[:location] }).
+                order(start_at: :desc).page(params[:page]).this_weeks
     @event_provinces = Location.joins(:events).pluck(:province).uniq
     @event_categories = Event.categories.keys
     render :index
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,91 +1,59 @@
 class PagesController < ApplicationController
   def index
     @events = Event.all.this_weeks.most_recent(50)
-    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
-      marker.lat event.latitude
-      marker.lng event.longitude
-      marker.title event.title
-      marker.infowindow render_to_string(:partial => "/events/infowindow", :locals => { :event_title => event.title, :event_url => event_path(event), event_date: event.start_at.strftime("วันที่: %-d/%-m/%y เวลา %H:%M") })
-      marker.json({
-        :clickable => true,
-        :center => [event.latitude, event.longitude]
-      })
-      marker.picture({
-        :width   => 32,
-        :height  => 37,
-        :anchor  => [10, 10],
-        :url     => view_context.image_path("#{event.category}.png")
-      })
-    end
+    @hash = build_markers(@events)
   end
 
   def map
     @events = Event.all.this_weeks.most_recent(50)
     @event_provinces = Location.joins(:events).pluck(:province).uniq
     @event_categories = Event.categories.keys
-    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
-      marker.lat event.latitude
-      marker.lng event.longitude
-      marker.title event.title
-      marker.infowindow render_to_string(:partial => "/events/infowindow", :locals => { :event_title => event.title, :event_url => event_path(event), event_date: event.start_at.strftime("วันที่: %-d/%-m/%y เวลา %H:%M") })
-      marker.json({
-        :clickable => true,
-        :center => [event.latitude, event.longitude]
-      })
-      marker.picture({
-        :width   => 32,
-        :height  => 37,
-        :anchor  => [10, 10],
-        :url     => view_context.image_path("#{event.category}.png")
-      })
-    end
+    @hash = build_markers(@events)
   end
 
   def map_with_location
-    @events = Event.joins(:location).where(locations: { province: params[:location] }).order(start_at: :desc).this_weeks.most_recent(20)
-    @event_provinces = Location.joins(:events).pluck(:province).uniq
+    @events = Event.joins(:location).
+                where(locations: { province: params[:location] }).
+                order(start_at: :desc).this_weeks.most_recent(20)
+    @event_provinces = Location.joins(:events).
+                        pluck(:province).uniq - [params[:location]]
     @event_categories = Event.categories.keys
-
-    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
-      marker.lat event.latitude
-      marker.lng event.longitude
-      marker.title event.title
-      marker.infowindow render_to_string(:partial => "/events/infowindow", :locals => { :event_title => event.title, :event_url => event_path(event), event_date: event.start_at.strftime("วันที่: %-d/%-m/%y เวลา %H:%M") })
-      marker.json({
-        :clickable => true,
-        :center => [event.latitude, event.longitude]
-      })
-      marker.picture({
-        :width   => 32,
-        :height  => 37,
-        :anchor  => [10, 10],
-        :url     => view_context.image_path("#{event.category}.png")
-      })
-    end
+    @hash = build_markers(@events)
     render :map
   end
 
   def map_with_category
-    @events = Event.where(category: params[:category]).order(start_at: :desc).this_weeks.most_recent(20)
+    @events = Event.where(category: params[:category]).
+                order(start_at: :desc).this_weeks.most_recent(20)
     @event_provinces = Location.joins(:events).pluck(:province).uniq
     @event_categories = Event.categories.keys - [params[:category]]
+    @hash = build_markers(@events)
+    render :map
+  end
 
-    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
+  private
+
+  def build_markers(events)
+    Gmaps4rails.build_markers(events) do |event, marker|
       marker.lat event.latitude
       marker.lng event.longitude
       marker.title event.title
-      marker.infowindow render_to_string(:partial => "/events/infowindow", :locals => { :event_title => event.title, :event_url => event_path(event), event_date: event.start_at.strftime("วันที่: %-d/%-m/%y เวลา %H:%M") })
+      marker.infowindow render_to_string(
+        partial: "/events/infowindow",
+        locals: { event_title: event.title,
+          event_url: event_path(event),
+          event_date: event.start_at.strftime("วันที่: %-d/%-m/%y เวลา %H:%M")}
+      )
       marker.json({
-        :clickable => true,
-        :center => [event.latitude, event.longitude]
+        clickable: true,
+        center: [event.latitude, event.longitude]
       })
       marker.picture({
-        :width   => 32,
-        :height  => 37,
-        :anchor  => [10, 10],
-        :url     => view_context.image_path("#{event.category}.png")
+        width:  32,
+        height: 37,
+        anchor: [10, 10],
+        url:    view_context.image_path("#{event.category}.png")
       })
     end
-    render :map
   end
 end
