cd7aba8fb16f83aa4dbd9b28fbc88b1a32f8102c
eakmotion <eakpun@gmail.com>
2017-03-05 00:33:55 +0700

    Set header to support CORS for API

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::EventsController < ApplicationController
+  before_action :set_headers
+
   def index
     @events = Event.order(start_at: :desc).
                 this_weeks.
@@ -44,5 +46,14 @@ class Api::V1::EventsController < ApplicationController
                 this_weeks.
                 page(params[:page])
     render :index, status: :ok
+  end
+
+  private
+  
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
