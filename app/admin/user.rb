57db9fcf0642e866a9f73fb33d6307b3d25e5925
eakmotion <eakpun@gmail.com>
2017-04-04 16:49:22 +0700

    Update user dashboard on admin page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,21 @@
 ActiveAdmin.register User do
+  actions :index, :show
+
   index do
     selectable_column
     id_column
     column :email
     column :full_name
     column :postal_code
+    column :district do |obj|
+      obj.location.district
+    end
+    column :province do |obj|
+      obj.location.province
+    end
+    column :disaster do |obj|
+      pluralize(obj.location.events.count, 'time')
+    end
     column :last_sign_in_at
     column :sign_in_count
     actions
@@ -12,4 +23,6 @@ ActiveAdmin.register User do
 
   filter :email
   filter :full_name
+  filter :location, as: :select,
+          collection: -> { Location.all.map{ |l| [l.postal_code, l.id] } }
 end
